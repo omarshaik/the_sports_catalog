12d83ad6da8455ab1af4347fd7fe49be97d4eb7e
Omar Shaik <omarshaik2011@u.northwestern.edu>
2016-04-17 22:00:11 -0700

    Add an initial user to the initial catalog categories and items.

--- categories.py
+++ categories.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database_setup import Category, Base, Item
+from database_setup import Category, Base, Item, User
 
 engine = create_engine('sqlite:///catalog.db')
 # Bind the engine to the metadata of the Base class so that the
@@ -18,43 +18,47 @@ DBSession = sessionmaker(bind=engine)
 # session.rollback()
 session = DBSession()
 
-category = Category(name="Baseball")
+admin = User(name="Omar Shaik", email="omshaik10@gmail.com")
+session.add(admin)
+session.commit()
+
+category = Category(user_id=1, name="Baseball")
 session.add(category)
 session.commit()
 
-item = Item(name="Baseball Bat", description="The tool used to hit baseballs. Can be customized and made from many different kinds of wood.", category=category)
+item = Item(user_id=1, name="Baseball Bat", description="The tool used to hit baseballs. Can be customized and made from many different kinds of wood.", category=category)
 session.add(item)
 session.commit()
 
-item = Item(name="Baseball", description="The ball with which baseball is played.", category=category)
+item = Item(user_id=1, name="Baseball", description="The ball with which baseball is played.", category=category)
 session.add(item)
 session.commit()
 
-item = Item(name="Baseball Glove", description="The tool used to catch baseballs.", category=category)
+item = Item(user_id=1, name="Baseball Glove", description="The tool used to catch baseballs.", category=category)
 session.add(item)
 session.commit()
 
-category = Category(name="Hockey")
+category = Category(user_id=1, name="Hockey")
 session.add(category)
 session.commit()
 
-item = Item(name="Hockey Stick", description="The tool used to hit hockey pucks. Also a term for insane growth.", category=category)
+item = Item(user_id=1, name="Hockey Stick", description="The tool used to hit hockey pucks. Also a term for insane growth.", category=category)
 session.add(item)
 session.commit()
 
-item = Item(name="Hockey Puck", description="A black, disk-like object. It is like a snitch in Quidditch.", category=category)
+item = Item(user_id=1, name="Hockey Puck", description="A black, disk-like object. It is like a snitch in Quidditch.", category=category)
 session.add(item)
 session.commit()
 
-category = Category(name="Basketball")
+category = Category(user_id=1, name="Basketball")
 session.add(category)
 session.commit()
 
-item = Item(name="Basketball", description="An orange, spherical object. It is bounced, often in creative and treacherous ways.", category=category)
+item = Item(user_id=1, name="Basketball", description="An orange, spherical object. It is bounced, often in creative and treacherous ways.", category=category)
 session.add(item)
 session.commit()
 
-item = Item(name="Basketball Hoop", description="The object in which the basketball is thrown.", category=category)
+item = Item(user_id=1, name="Basketball Hoop", description="The object in which the basketball is thrown.", category=category)
 session.add(item)
 session.commit()
 
