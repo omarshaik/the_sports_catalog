bdc986efa2cb8486632bf4b505fa504b574d8a06
Omar Shaik <omarshaik2011@u.northwestern.edu>
2016-04-23 21:22:48 -0700

    Formatting changes.

--- application.py
+++ application.py
@@ -123,23 +123,22 @@ def gconnect():
 
 # User Helper Functions
 def create_user(login_session):
-	new_user = User(name=login_session['username'], email=login_session[
-                   'email'], picture=login_session['picture'])
+	new_user = User(name=login_session['username'], email=login_session['email'], picture=login_session['picture'])
 	session.add(new_user)
 	session.commit()
 	user = session.query(User).filter_by(email=login_session['email']).one()
 	return user.id
 
 def get_user_info(user_id):
-    user = session.query(User).filter_by(id=user_id).one()
-    return user
+	user = session.query(User).filter_by(id=user_id).one()
+	return user
 
 def get_user_id(email):
-    try:
-    	user = session.query(User).filter_by(email=email).one()
-    	return user.id
-    except:
-    	return None
+	try:
+		user = session.query(User).filter_by(email=email).one()
+		return user.id
+	except:
+		return None
 
 @app.route('/gdisconnect')
 def gdisconnect():
