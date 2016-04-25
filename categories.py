d59641428b3434b7780c6e98ebf57b7666a8cbf1
Omar Shaik <omarshaik2011@u.northwestern.edu>
2016-04-25 02:15:54 -0700

    Add more sports items to the default db.

--- categories.py
+++ categories.py
@@ -62,5 +62,45 @@ item = Item(user_id=1, name="Basketball Hoop", description="The object in which 
 session.add(item)
 session.commit()
 
+category = Category(user_id=1, name="Football")
+session.add(category)
+session.commit()
+
+item = Item(user_id=1, name="Football", description="A brown, oddly-shaped object. It is thrown in a spiral.", category=category)
+session.add(item)
+session.commit()
+
+item = Item(user_id=1, name="Helmet", description="The object that fails to prevent concussions. It is worn on the head.", category=category)
+session.add(item)
+session.commit()
+
+category = Category(user_id=1, name="Futbol (Soccer)")
+session.add(category)
+session.commit()
+
+item = Item(user_id=1, name="Soccer Ball", description="A white and black, spherical object. It is kicked around and at people, most often the goalie.", category=category)
+session.add(item)
+session.commit()
+
+category = Category(user_id=1, name="Volleyball")
+session.add(category)
+session.commit()
+
+item = Item(user_id=1, name="Volleyball", description="A spherical object. It is slapped around by athletes. Often mistaken for a soccer ball.", category=category)
+session.add(item)
+session.commit()
+
+category = Category(user_id=1, name="Golf")
+session.add(category)
+session.commit()
+
+item = Item(user_id=1, name="Golf Ball", description="A spherical object. It is hit impressively long distances. May look like a miniature volleyball to the untrained eye.", category=category)
+session.add(item)
+session.commit()
+
+item = Item(user_id=1, name="Golf Club", description="A club that meets to play golf, usually on the weekends.", category=category)
+session.add(item)
+session.commit()
+
 
 
